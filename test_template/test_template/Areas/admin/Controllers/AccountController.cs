fcb5ccd483098609b9a474f624efc2614f0a8a03
tanphatkyhoa@gmail.com <tanphatkyhoa@gmail.com>
2019-10-08 20:20:18 +0700

    chg

---
+++ test_template/test_template/Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using test_template.Models.function;
+using test_template.Models.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace test_template.Areas.admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: admin/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Account accont)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new AccountDao();
+                int id = dao.insert(accont);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm không thành công");
+                }
+            }
+                
+            return View(accont);
+        }
+    }
+}
---
+++ test_template/test_template/Areas/admin/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using test_template.Models.function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test_template.Areas.admin.Models;
+using test_template.common;
+
+namespace test_template.Areas.admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new AccountDao();
+                int result = dao.login(model.UserName, model.Passwork);
+                if (result == 1)
+                {
+                    var userSession = new AccountLogin();
+                    var user = dao.GetbyId(model.UserName);
+                    userSession.AccountName = user.Username;
+                    userSession.AccountId = user.ID;
+                    Session.Add(CommonConstanr.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Account");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại!");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản bị khóa");
+                }
+                else
+                    ModelState.AddModelError("", "Mật khẩu không đúng");
+            }
+            return View("Index");
+
+        }
+    }
+}
